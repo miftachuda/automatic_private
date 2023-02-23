bdaea854bc406504fe5b6f1beb3e0f82867c49fd
Miftachul Huda <miftachulcc@gmail.com>
2023-02-23 19:45:53 +0700

    migrate to luxon fix timezone

--- exe.js
+++ exe.js
@@ -1,6 +1,5 @@
 const fs = require('fs/promises');
 const lodash = require("lodash")
-const moment = require("moment")
 const axios = require('axios');
 const qs = require('qs');
 const luxon = require("luxon")
@@ -35,17 +34,7 @@ function getPeriod(min) {
     }
 }
 
-function checkShift() {
-    var now = moment("2023-02-23 05:00");
-    var end = moment("2021-12-22");
-    console.log(end.toString())
-    var duration = moment.duration(now.diff(end));
-    console.log(duration)
-    var day = Math.trunc(duration.asDays()) % 12
-    var minutes = Math.trunc(duration.asMinutes()) % 1440
-    return listshift[day][getPeriod(minutes)];
-}
-console.log(checkShift())
+
 function checkShiftLx() {
     const now = luxon.DateTime.now().setZone("Asia/jakarta");
     console.log(now.toString())
@@ -56,21 +45,6 @@ function checkShiftLx() {
     const day = Math.trunc(diff.days) % 12;
     const minutes = Math.trunc(diff.minutes) % 1440;
     return listshift[day][getPeriod(minutes)];
-}
-
-function checkShiftPeriod() {
-    var hours = moment().hours()
-    if (hours < 4) {
-        return "00:00"
-    } else if (hours < 8) {
-        return "04:00"
-    } else if (hours < 12) {
-        return "08:00"
-    } else if (hours < 16) {
-        return "12:00"
-    } else if (hours < 20) {
-        return "16:00"
-    } else return "20:00"
 }
 
 function checkShiftPeriodLx() {
